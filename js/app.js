be32cce1bf2bb0e40c2aa5fd32261da033526c1f
David Isles <diisles@yahoo.com>
2016-06-29 18:27:47 -0700

    created game logic

---
+++ js/app.js
@@ -0,0 +1,35 @@
+console.log("This is your javascript console. If you see this message, you're good to go!");
+
+
+function task_one(){
+	console.log("Task one: use document.getElementById to select the paragraph that says 'javascript can be hard'. Change it to 'javascript is fun!' ");
+
+}
+
+function task_two(){
+	console.log("Task two: use document.getElementsByClassName to select the title. Change the title's text to 'I LOVE JAVASCRIPT!'");
+}
+
+function task_three(){
+	console.log("Task three: use document.getElementsByTagName to select the body and change the color of all text on the page to yellow");
+	
+}
+
+function task_four(){
+	console.log("Task four: use document.getElementsByTagName to select the sadface image. Change the source to this: 'http://upload.wikimedia.org/wikipedia/en/6/6f/Smiley_Face.png'");
+
+}
+
+function task_five(){
+	console.log("Task five: This is a tricky one. Use document.getElementsByTagName to select the body again. We want to give it a background image of this: 'http://hdpictureshq.com/sites/default/files/rainbows-images-hq-pictures-hd-17744.jpg'");
+}
+
+///////////// Write your own JS below!  /////////////////////
+
+
+
+
+
+
+
+
